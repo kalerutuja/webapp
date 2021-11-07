3ddcd8ec6c24e6c424d19ac6504588111d101b6d
kalerutuja <ruturs26@gmail.com>
2021-11-07 02:42:03 -0800

    added code deploy scripts

--- scripts/StopApplication.sh
+++ scripts/StopApplication.sh
@@ -2,7 +2,7 @@
 
 PyPID=`/usr/bin/ps aux | grep "python3 main.py" | grep -v grep | awk '{print $2}'`
 if [ $PyPID ]; then
-    kill -15 $pyPID
+    kill -n 15 $pyPID
 fi
 
-kill -15 $pyPID
+kill -n 15 $pyPID
