f34632785c02ae23e88324857e7bd7c2debadbb2
kalerutuja <ruturs26@gmail.com>
2021-11-19 10:52:01 -0800

    adding cloudwatch config

---
+++ scripts/CopyCloudWatchConfig.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PyPID=`/usr/bin/ps aux | grep "python3 main.py" | grep -v grep | awk '{print $2}'`
+if [ $PyPID ]; then
+    echo $PyPID
+    kill -n 15 $PyPID
+fi
+sudo systemctl stop amazon-cloudwatch-agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:cloudwatch-config.json \
+    -s
+sudo systemctl start amazon-cloudwatch-agent
