a65fd81f1b59ac694a8837c44dd367121604b0be
kalerutuja <ruturs26@gmail.com>
2021-11-20 03:50:47 -0800

    adding config

--- scripts/RunApplicationAfter.sh
+++ scripts/RunApplicationAfter.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu/webapp
 pkill -f 'python3 main.py'
 tee >(logger) <<< "webapp: stopped"
-sudo pkill apache2
-tee >(logger) <<< "apache2: stopped"
+sudo pkill -f 'http.server'
+tee >(logger) <<< "python webserver: stopped"
 sudo systemctl restart amazon-cloudwatch-agent
 tee >(logger) <<< "amazon-cloudwatch-agent: restarted"
