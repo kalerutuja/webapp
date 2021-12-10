ff6f5dfca37f43073958f3d450926eb55434dab7
kalerutuja <ruturs26@gmail.com>
2021-12-10 02:47:16 -0800

    adding update

--- app/__init__.py
+++ app/__init__.py
@@ -20,11 +20,12 @@ with open('/opt/resources') as f:
 
 def create_app():
     webapp.config['SECRET_KEY'] = 'gjasdgasgdghjdjsdgksj856876s5s7d57asd55'
+    ssl_args = {'ssl': {'ca': 'us-east-1-bundle.pem'}}
     # application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://csye6225:csye6225@csye6225.c6b2oknmn4xd.us-east-1.rds.amazonaws.com/csye6225'
     # webapp.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://csye6225:csye6225@csye6225.c6b2oknmn4xd.us-east-1.rds.amazonaws.com/csye6225'
-    webapp.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{credentials[1]}:{credentials[2]}@{credentials[4]}/{credentials[3]}'
+    webapp.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{credentials[1]}:{credentials[2]}@{credentials[4]}/{credentials[3]},connect_args=ssl_args'
     webapp.config['SQLALCHEMY_BINDS'] = {
-        'slave': f'mysql://{credentials[1]}:{credentials[2]}@{credentials[5]}/{credentials[3]}'
+        'slave': f'mysql+mysqlconnector://{credentials[1]}:{credentials[2]}@{credentials[5]}/{credentials[3]},connect_args=ssl_args'
     }
     webapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
